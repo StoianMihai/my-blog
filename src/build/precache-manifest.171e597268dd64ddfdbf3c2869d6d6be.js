3c17dabe4344703411fde3f50dbb14f5232d215b
Stoian Mihai <mihai_stoian21@yahoo.com>
2020-08-21 15:11:13 +0300

    First commit

---
+++ src/build/precache-manifest.171e597268dd64ddfdbf3c2869d6d6be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025e4653ce4d67aada040adb286e55ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01848000161e3e0b5b38",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "92369fd5f11c815c5dbd",
+    "url": "/static/js/2.17807cbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17807cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01848000161e3e0b5b38",
+    "url": "/static/js/main.9bd94df9.chunk.js"
+  },
+  {
+    "revision": "f23e36680813166ba6f7",
+    "url": "/static/js/runtime-main.05ce6b3a.js"
+  }
+]);
